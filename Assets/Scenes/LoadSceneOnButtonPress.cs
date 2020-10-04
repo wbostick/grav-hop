63abbef368269771d6124b326faa481ed5f37169
William Bostick <wbostick@umich.edu>
2020-10-04 14:31:32 -0400

    menu

---
+++ Assets/Scenes/LoadSceneOnButtonPress.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class LoadSceneOnButtonPress : MonoBehaviour
+{
+    public string sceneToLoad = "Menu";
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(() => {SceneManager.LoadScene(sceneToLoad);});
+    }
+}
