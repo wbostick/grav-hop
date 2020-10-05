e50fb81dd4d10962b05067aa3e79ad0af8425478
William Bostick <wbostick@umich.edu>
2020-10-05 16:27:23 -0400

    player gravity allows to cling to surface

--- Assets/Grounded.cs
+++ Assets/Grounded.cs
@@ -6,8 +6,8 @@ public class Grounded : MonoBehaviour
 {
     float gravityStrength;
     Rigidbody2D rigid;
+    public Transform groundCheckEndpoint;
 
-    public float isGroundedTolerance = .1f;
     private void Awake()
     {
         gravityStrength = Physics2D.gravity.magnitude;
@@ -22,18 +22,32 @@ public class Grounded : MonoBehaviour
     bool RotateIfOnGround()
     {
         RaycastHit2D hit = GetGround();
-        if (hit)
+        if (hit.collider)
         {
             Physics2D.gravity = -hit.normal.normalized * gravityStrength;
-            rigid.SetRotation(Quaternion.LookRotation(transform.up, hit.normal));
+            rigid.SetRotation(Quaternion.LookRotation(Vector3.forward, hit.normal.normalized));
             return true;
         }
+        else
+        {
+            return false;
+        }
 
-        return false;
     }
 
     RaycastHit2D GetGround()
+    {
+        RaycastHit2D[] results = new RaycastHit2D[2];
+        Vector2 direction = groundCheckEndpoint.position - transform.position;
+        Physics2D.Raycast(transform.position, direction, new ContactFilter2D(), results, direction.magnitude);
+        foreach (RaycastHit2D result in results)
         {
-            return Physics2D.Raycast(transform.position, Vector2.down, isGroundedTolerance);
+            if (result.collider && result.collider.gameObject != gameObject)
+            {
+                return result;
+            }
         }
+        
+        return new RaycastHit2D();
     }
+}
