94abf491312ab74ef4a39cc0c0babbecf4407ebc
William Bostick <wbostick@umich.edu>
2020-10-06 18:55:03 -0400

    add buttons to return to menu and reset

---
+++ Assets/Scenes/ResetScene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetScene : MonoBehaviour
+{
+    public void ResetScenePlease()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
